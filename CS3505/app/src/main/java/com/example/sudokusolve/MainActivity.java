32c77a3a375c3935117b55e7ad18c7ceb9a117c6
pat <paddymbarry@gmail.com>
2022-02-07 18:01:29 +0000

    added buttons for hint/solution

--- CS3505/app/src/main/java/com/example/sudokusolve/MainActivity.java
+++ CS3505/app/src/main/java/com/example/sudokusolve/MainActivity.java
@@ -6,19 +6,18 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int CAMERA_ACTION_CODE=1;
     ImageView imageView;
     FloatingActionButton button;
 
@@ -34,14 +33,14 @@ public class MainActivity extends AppCompatActivity {
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{
                     Manifest.permission.CAMERA
-            }, 1);
+            }, CAMERA_ACTION_CODE);
         }
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 1);
+                startActivityForResult(intent, CAMERA_ACTION_CODE);
             }
         });
     }
@@ -49,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode==1 && resultCode==RESULT_OK && data!=null) {
+        if (requestCode==CAMERA_ACTION_CODE && resultCode==RESULT_OK && data!=null) {
             Bundle bundle = data.getExtras();
             Bitmap finalPhoto = (Bitmap) bundle.get("data");
             imageView.setImageBitmap(finalPhoto);
